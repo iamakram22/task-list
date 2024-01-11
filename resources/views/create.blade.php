4a53f100428f1f6b852bf58136ba90298185e10e
Akram <iamakram22@gmail.com>
2024-01-11 17:54:31 +0530

    created add task form and post route

---
+++ resources/views/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('heading', 'Add Task')
+
+@section('content')
+    <form action="{{route('tasks.store')}}" method="post">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" />
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="3"></textarea>
+        </div>
+        <div>
+            <label for="long_description">Long Description</label>
+            <textarea name="long_description" id="long_description" rows="10"></textarea>
+        </div>
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 @section('heading', 'Hello, welcome to Task list')
 
 @section('content')
+    <a href="{{route('tasks.create')}}">Create task</a>
     @forelse ($tasks as $task)
     <div>
         <h2><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></h2>
